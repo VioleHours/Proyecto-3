fcd76375f64b6ed0e0dfe34b893dfe78f883ed43
Violeta <holasoyviole@gmail.com>
2023-09-27 22:27:05 -0300

    base

--- flutter-feed/src/app/page.tsx
+++ flutter-feed/src/app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: posts } = await supabase.from('posts').select()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       HOLA TWITTER
+      <pre>
+        {
+          JSON.stringify(posts, null, 2)
+        }
+      </pre>
     </main>
   )
 }
