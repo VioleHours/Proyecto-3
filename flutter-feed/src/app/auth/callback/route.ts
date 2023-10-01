57b358eb069b26ab987623582d8579346001a100
VioletaHoursDAS <violeta@dascalendar.com>
2023-10-01 00:17:14 -0300

    actualización

---
+++ flutter-feed/src/app/auth/callback/route.ts
@@ -0,0 +1,19 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse, type NextRequest } from "next/server";
+import { cookies } from "next/headers";
+
+export const dynamic = 'force-dynamic';
+
+export async function GET( request : NextRequest) {
+    const requestURL = new URL(request.url);
+    const code = requestURL.searchParams.get('code'); //accede al query string sin tener que utilizar regex
+    
+    if (code !== null) {
+        const supabase = createRouteHandlerClient({ cookies });
+        //usando el codigo que le pasamos por URL
+        // nos devuelve la sesion de usuario
+        await supabase.auth.exchangeCodeForSession(code);
+    }
+
+    return NextResponse.redirect(requestURL.origin); //redirecciona a la pagina que estaba antes de loguearse
+}
--- flutter-feed/src/app/page.tsx
+++ flutter-feed/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import { AuthButton } from './components/auth-button'
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies })
@@ -7,7 +8,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      HOLA TWITTER
+      <AuthButton />
       <pre>
         {
           JSON.stringify(posts, null, 2)
